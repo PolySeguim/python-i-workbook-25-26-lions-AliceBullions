b9927709e66c3f8132bb9431b649abed088dbc1e
AliceBullions <soccertodance@gmail.com>
2025-10-03 13:40:56 -0400

    10/3/25

--- fall_trutles.py
+++ fall_trutles.py
@@ -21,27 +21,74 @@ def drawClock():
         turtle1.right(30)
         turtle1.forward(150)
 
+def drawClock(t):
+    turtle1.stamp()
+    turtle1.penup()
+    turtle1.forward(150)
+    for i in range(12):
+        turtle1.pendown()
+        turtle1.stamp()
+        turtle1.penup()
+        turtle1.backward(150)
+        turtle1.right(30)
+        turtle1.forward(150)
+
 def specialSquare():
     for i in range(1000):
         turtle1.forward((2+i)*2)
         turtle1.left(90)
 
 def swirlySquare():
-    screen.bgcolor("maroon")
-    turtle1.color("darkorange")
-    turtle1.pensize(3)
+    screen.bgcolor("darkorange")
+    turtle1.color("maroon")
+    turtle1.pensize(10)
     turtle1.speed(1000)
-    for i in range(300):
-         turtle1.forward((0+i))
-         turtle1.left(67)
-swirlySquare()
+    for i in range(750):
+         turtle1.forward((0+i)*2)
+         turtle1.left(91)
+
+def multiColorSquare(t, sz, c):
+    t.pensize(3)
+    for i in c:
+        t.color(i)
+        t.forward(sz)
+        t.left(90)
+def getThickness():
+    thickness=int(input("Enter the thickness of the turtles"))
+    return thickness 
+def test():
+    test_turtle=turtle.Turtle()
+    test_turtle.color("pink")
+    color1=["red", "purple","hotpink","blue"]
+    color2=["black", "orange", "green", "yellow"]
+    color3=["purple", "red", "grey", "brown"]
+    for i in range(200):
+        multiColorSquare(test_turtle, i*10, c)
+    
+
+def drawSquare(t, side):
+    for i in range(4):
+        t.forward(side)
+        t.right(90)
+
+
+
+def testSpace():
+   testy_turtle=turtle.Turtle
+   for i in range(5):
+       drawSquare(testy_turtle, 20)
+       testy_turtle.penup()
+       testy_turtle.forward(10)
+       testy_turtle.pendown()
+
+testSpace()
+
 
 
 
   
-
+screen.listen()
+screen.mainloop()
     
 
 
-screen.listen()
-screen.mainloop()
